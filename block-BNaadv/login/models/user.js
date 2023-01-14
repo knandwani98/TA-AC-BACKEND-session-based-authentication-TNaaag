a9ff27b3b2afa35bfc36f0d756cc566078b13a32
Kushal Nandwani <kushalnandwani98@gmail.com>
2023-01-14 18:43:29 +0530

    assn1

---
+++ block-BNaadv/login/models/user.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minLength: 5 },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  if (this.password && this.isModified("password")) {
+    bcrypt.hash(this.password, 10, (err, hashed) => {
+      if (err) return next(err);
+      this.password = hashed;
+      return next();
+    });
+  } else {
+    return next();
+  }
+});
+
+userSchema.methods.verifyPassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, result) => {
+    return cb(err, result);
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
